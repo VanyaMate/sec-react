138f6e60f9107650d4302a187cb53d4537f2df4b
VanyaMate <buhu653@mail.ru>
2024-06-27 19:19:39 +0300

    update type

--- index.ts
+++ index.ts
@@ -2,6 +2,6 @@ import { Store } from '@vanyamate/sec';
 import { useSyncExternalStore } from 'react';
 
 
-export const useStore = function <T> (store: Store<T>) {
+export const useStore = function <T> (store: Store<T>): T {
     return useSyncExternalStore(store.subscribe, store.get);
 };
